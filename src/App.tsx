25d8a70eb9be7b79b1e2621e7e9e80be643d3e3c
Satish Raut <satshraut@gmail.com>
2025-06-03 21:00:47 +0530

    Resolve the login issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import Compiler from './components/Compiler';
+import AuthPage from './components/auth/AuthPage';
+import UsernameSetup from './components/auth/UsernameSetup';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Compiler />
+      <AuthProvider>
+        <Routes>
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/username-setup" element={
+            <ProtectedRoute>
+              <UsernameSetup />
+            </ProtectedRoute>
+          } />
+          <Route path="/compiler" element={
+            <ProtectedRoute>
+              <Compiler />
+            </ProtectedRoute>
+          } />
+          <Route path="/" element={<Compiler />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
